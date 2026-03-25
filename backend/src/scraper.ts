3aebec4f7427e422ddbd65cc6acd7419adb37d9f
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 00:28:16 +0100

    Update scraper.ts

--- backend/src/scraper.ts
+++ backend/src/scraper.ts
@@ -109,3 +109,4 @@ export class LinkedInScraper {
           const width = img.naturalWidth || img.width || 0;
           const height = img.naturalHeight || img.height || 0;
           if (Math.max(width, height) < 200) return;
+
