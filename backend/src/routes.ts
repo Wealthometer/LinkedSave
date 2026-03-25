a45ffd3d692a0afefc364fe8c4c7bb66858b28cd
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 00:02:37 +0100

    Update routes.ts

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -73,3 +73,4 @@ router.get("/download", async (req: Request, res: Response) => {
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
         Referer: "https://www.linkedin.com/",
       },
+      timeout: 30_000,
