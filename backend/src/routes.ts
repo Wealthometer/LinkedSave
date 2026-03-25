92ea74c1964dc82a5c99624a4f04e21031b7ea2c
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 00:02:35 +0100

    Update routes.ts

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -72,3 +72,4 @@ router.get("/download", async (req: Request, res: Response) => {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
         Referer: "https://www.linkedin.com/",
+      },
