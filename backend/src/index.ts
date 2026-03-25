fc26efd27f400bdc4d840cd4de0e0de43240a47b
Wealthometer <wealthhajoh87@gmail.com>
2026-03-25 23:54:55 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,3 +8,4 @@ const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
