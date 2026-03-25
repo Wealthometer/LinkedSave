7087f184e653f60e52110b703d767f749944d0f1
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 00:15:39 +0100

    Update scraper.ts

--- backend/src/scraper.ts
+++ backend/src/scraper.ts
@@ -2,3 +2,4 @@ import puppeteer, { Browser, Page } from "puppeteer";
 import { MediaItem, LinkedInCookie } from "./types";
 
 const USER_AGENT =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
