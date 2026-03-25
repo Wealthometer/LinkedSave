2f6c639ab18d8e02413824587dc2a06d8e16edfa
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 00:18:06 +0100

    Update scraper.ts

--- backend/src/scraper.ts
+++ backend/src/scraper.ts
@@ -36,3 +36,4 @@ export class LinkedInScraper {
 
     // Stealth: mask webdriver flag
     await page.evaluateOnNewDocument(() => {
+      Object.defineProperty(navigator, "webdriver", { get: () => false });
