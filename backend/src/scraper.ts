8dd607e6ed80d6d97f441e0c2d0b300bc14adad7
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 00:31:05 +0100

    Update scraper.ts

--- backend/src/scraper.ts
+++ backend/src/scraper.ts
@@ -157,3 +157,4 @@ export class LinkedInScraper {
         videoSelectors.forEach((sel) => {
           document.querySelectorAll<HTMLVideoElement>(sel).forEach((vid) => {
             const dataSources = vid.getAttribute("data-sources");
+            if (dataSources) {
