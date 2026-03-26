8d85814d9c40ac54c2cf2be1182288804c87278d
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 16:17:20 +0100

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -19,3 +19,4 @@ export const saveSession = (cookiesJson: string): Promise<{ message: string }> =
 };
 
 export const clearSession = (): Promise<{ message: string }> =>
+  api.delete<{ message: string }>("/session").then((r) => r.data);
