c14552f5a52fe9ceed3c77df69f8c98089d2e81d
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 16:17:59 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,3 +2,4 @@ import { useState } from "react";
 import { Header } from "./components/Header";
 import { UrlInput } from "./components/UrlInput";
 import { MediaGrid } from "./components/MediaGrid";
+import { SessionPanel } from "./components/SessionPanel";
