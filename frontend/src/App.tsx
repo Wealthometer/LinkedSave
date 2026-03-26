3fbca70c49beec5f968e101a4662f8583cac0a51
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 16:24:21 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -36,3 +36,4 @@ export default function App() {
           type: "error",
           message:
             result.message ||
+            "No media found. Try adding session cookies for private posts.",
