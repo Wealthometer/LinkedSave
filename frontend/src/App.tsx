1e77b883e2a96e2533057087e05671e7af97526e
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 16:18:19 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,3 +10,4 @@ import { MediaItem, StatusState } from "./types";
 export default function App() {
   const [url, setUrl] = useState("");
   const [loading, setLoading] = useState(false);
+  const [media, setMedia] = useState<MediaItem[]>([]);
