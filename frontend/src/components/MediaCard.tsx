1c18de64e98e325f09803ba0c481f3d4d3d9da38
Wealthometer <wealthhajoh87@gmail.com>
2026-03-26 19:00:42 +0100

    Update MediaCard.tsx

--- frontend/src/components/MediaCard.tsx
+++ frontend/src/components/MediaCard.tsx
@@ -53,3 +53,4 @@ export function MediaCard({ item, animationDelay = 0 }: Props) {
             {isVideo ? <Video size={10} /> : <Image size={10} />}
             {item.type}
           </span>
+        </div>
