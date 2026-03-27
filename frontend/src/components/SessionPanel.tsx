4eef89fc90178bd6593f246e38821e7d40f52a97
Wealthometer <wealthhajoh87@gmail.com>
2026-03-27 01:08:44 +0100

    Update SessionPanel.tsx

--- frontend/src/components/SessionPanel.tsx
+++ frontend/src/components/SessionPanel.tsx
@@ -111,6 +111,7 @@ export function SessionPanel() {
                   ? "text-danger"
                   : "text-success"
             >
+              {feedback}
             </p>
 
             <button
